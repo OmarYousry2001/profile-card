460b0c75552dc3c25e7612bef58037f76a83dd09
omar <OmarYoussry2001>
2025-07-26 10:53:42 +0300

    Finished the profile card project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
+  Name: string = 'Omar';
+  Age: number = 30;
+  Description: string =
+    'A passionate developer with a love for coding and technology.';
   protected title = 'profile-card';
 }
